832cad1b51dea9493c682ca673c87bd0b4fedd8e
Reni Ferreira <renirogete.exp@gmail.com>
2024-12-02 21:19:25 -0300

    Fixes em Tela Cliente

--- tela/tela_cliente.py
+++ tela/tela_cliente.py
@@ -2,6 +2,36 @@ import PySimpleGUI as sg
 
 
 class TelaCliente:
+    def __init__(self):
+        self.__window = None
+        self.init_opcoes()
+
+    def tela_opcoes(self) -> int:
+        self.init_opcoes()
+        button, values = self.__window.open()
+        opcao = 0
+
+        while invalid_input:
+            if not first_try:
+                if values['1']:
+                    opcao = 1
+                
+                if values['2']:
+                    opcao = 2
+
+                if values['3']:
+                    opcao = 3
+
+                if values['4']:
+                    opcao = 4
+
+                if values['0'] or button in (None, 'Cancelar'):
+                    opcao = 0
+
+                self.close()
+                return opcao
+
+    
     # def tela_opcoes(self) -> int:
     #     opcao_valida = False
     #     while not opcao_valida:
@@ -25,10 +55,7 @@ class TelaCliente:
     #     return opcao
 
 
-    def close(self):
-        self.__window.Close()
-
-    def init_components(self):
+    def init_opcoes(self):
         #sg.theme_previewer()
         sg.ChangeLookAndFeel('DarkTeal4')
         layout = [
@@ -38,98 +65,95 @@ class TelaCliente:
             [sg.Radio('Alterar Cliente',"RD1", key='2')],
             [sg.Radio('Listar Clientes',"RD1", key='3')],
             [sg.Radio('Excluir Cliente',"RD1", key='4')],
-            [sg.Radio('Sair da Tela Cliente',"RD1", key='6')],
+            [sg.Radio('Sair da Tela Cliente',"RD1", key='0')],
             [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
         ]
         self.__window =  sg.Window('Sistema de Clientes').Layout(layout)
 
-    def __init__(self):
-        self.__window = None
-        self.init_components()
-
-    def tela_opcoes(self) -> int:
-
-        self.init_components()
-        button, values = self.__window.Read()
-        invalid_input = True
-        first_try = True
-        opcao = 0
-
-        while invalid_input:
-            if not first_try:
-                if values['1']:
-                    opcao = 1
-                
-                if values['2']:
-                    opcao = 2
-
-                if values['3']:
-                    opcao = 3
-
-                if values['4']:
-                    opcao = 4
-
-                if values['5']:
-                    opcao = 5
-
-                if values['6']:
-                    opcao = 6
-
-                if values['0'] or button in (None, 'Cancelar'):
-                    opcao = 0
-
-                self.close()
-                return opcao
-
+    def close(self):
+        self.__window.Close()
                 
     def pega_dados_cliente(self) -> dict:
-        print("-------- DADOS CLIENTE ----------")
         
-        
-        nome = input("Nome: ")
-        while nome == "":
-            print("O nome não pode ser vazio.")
-            nome = input("Nome: ")
+        sg.ChangeLookAndFeel('DarkTeal4')
+        layout = [
+        [sg.Text('-------- DADOS CLIENTE ----------', font=("Helvica", 25))],
+        [sg.Text('Nome:', size=(15, 1)), sg.InputText('', key='nome')],
+        [sg.Text('CPF:', size=(15, 1)), sg.InputText('', key='cpf')],
+        [sg.Text('ID do Cliente:', size=(15, 1)), sg.InputText('', key='id_cliente')],
+        [sg.Text('Filmes vistos:', size=(15, 1)), sg.InputText('', key='filmes_vistos')],
+        [sg.Text('Sessões aguardando:', size=(15, 1)), sg.InputText('', key='sessoes_aguardando')],
+        [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+        ]
+        self.__window = sg.Window('Sistema de livros').Layout(layout)
+
+        button, values = self.open()
+        nome = values['nome']
+        telefone = values['telefone']
+        cpf = values['cpf']
+
+        self.close()
+        return {"nome": nome, "telefone": telefone, "cpf": cpf}
+
+        #print("-------- DADOS CLIENTE ----------")
+        #
+        #
+        #nome = input("Nome: ")
+        #while nome == "":
+        #    print("O nome não pode ser vazio.")
+        #    nome = input("Nome: ")
 
         
-        cpf = input("CPF (somente números): ")
-        while not self.valida_cpf(cpf):
-            print("CPF inválido. Digite exatamente 11 dígitos numéricos.")
-            cpf = input("CPF (somente números): ")
+        #cpf = input("CPF (somente números): ")
+        #while not self.valida_cpf(cpf):
+        #    print("CPF inválido. Digite exatamente 11 dígitos numéricos.")
+        #    cpf = input("CPF (somente números): ")
         
         
-        id_cliente = input("ID: ")
-        while not self.valida_id_cliente(id_cliente):
-            print("ID inválido! Deve ser um número inteiro.")
-            id_cliente = input("ID: ")
-        id_cliente = int(id_cliente)
+        #id_cliente = input("ID: ")
+        #while not self.valida_id_cliente(id_cliente):
+        #    print("ID inválido! Deve ser um número inteiro.")
+        #    id_cliente = input("ID: ")
+        #id_cliente = int(id_cliente)
 
         
-        filmes_vistos = input("Filmes vistos (separe por vírgula ou digite '0' se não houver): ")
-        filmes_vistos_lista = [] if filmes_vistos.strip() == "0" else [f.strip() for f in filmes_vistos.split(",")]
+        #filmes_vistos = input("Filmes vistos (separe por vírgula ou digite '0' se não houver): ")
+        #filmes_vistos_lista = [] if filmes_vistos.strip() == "0" else [f.strip() for f in filmes_vistos.split(",")]
 
         
-        sessoes_aguardando = input("Sessões aguardando (separe por vírgula ou digite '0' se não houver): ")
-        sessoes_aguardando_lista = [] if sessoes_aguardando.strip() == "0" else [s.strip() for s in sessoes_aguardando.split(",")]
+        #sessoes_aguardando = input("Sessões aguardando (separe por vírgula ou digite '0' se não houver): ")
+        #sessoes_aguardando_lista = [] if sessoes_aguardando.strip() == "0" else [s.strip() for s in sessoes_aguardando.split(",")]
 
-        return {
-            "nome": nome,
-            "cpf": cpf,
-            "id_cliente": id_cliente,
-            "filmes_vistos": filmes_vistos_lista,
-            "sessoes_aguardando": sessoes_aguardando_lista,
-        }
+        #return {
+        #    "nome": nome,
+        #    "cpf": cpf,
+        #    "id_cliente": id_cliente,
+        #    "filmes_vistos": filmes_vistos_lista,
+        #    "sessoes_aguardando": sessoes_aguardando_lista,
+        #}
 
     def mostra_cliente(self, dados_cliente: dict):
-        filmes_vistos = dados_cliente.get("filmes_vistos", [])
-        sessoes_aguardando = dados_cliente.get("sessoes_aguardando", [])
+        string_todos_clientes = ""
+        for dado in dados_amigo:
+            string_todos_clientes = string_todos_clientes + "NOME DO CLIENTE: " + dado["nome"] + '\n'
+            string_todos_clientes = string_todos_clientes + "CPF DO CLIENTE: " + str(dado["cpf"]) + '\n'
+            string_todos_clientes = string_todos_clientes + "ID DO CLIENTE: " + str(dado["id_cliente"]) + '\n\n'
+            string_todos_clientes = string_todos_clientes + "FILMES VISTOS DO CLIENTE: " + str(dado["filmes_vistos"]) + '\n\n'
+            string_todos_clientes = string_todos_clientes + "SESSÕES AGUARDANDO DO CLIENTE: " + str(dado["sessoes_aguardando"]) + '\n\n'
 
-        print("NOME DO CLIENTE:", dados_cliente["nome"])
-        print("CPF DO CLIENTE:", dados_cliente["cpf"])
-        print("ID DO CLIENTE:", dados_cliente["id_cliente"])
-        print("FILMES VISTOS DO CLIENTE:", ", ".join(filmes_vistos) if filmes_vistos else "Nenhum")
-        print("SESSOES AGUARDANDO DO CLIENTE:", ", ".join(sessoes_aguardando) if sessoes_aguardando else "Nenhuma")
-        print("\n")
+
+        sg.Popup('-------- LISTA DE AMIGOS ----------', string_todos_clientes)
+        
+        
+        #filmes_vistos = dados_cliente.get("filmes_vistos", [])
+        #sessoes_aguardando = dados_cliente.get("sessoes_aguardando", [])
+
+        #print("NOME DO CLIENTE:", dados_cliente["nome"])
+        #print("CPF DO CLIENTE:", dados_cliente["cpf"])
+        #print("ID DO CLIENTE:", dados_cliente["id_cliente"])
+        #print("FILMES VISTOS DO CLIENTE:", ", ".join(filmes_vistos) if filmes_vistos else "Nenhum")
+        #print("SESSOES AGUARDANDO DO CLIENTE:", ", ".join(sessoes_aguardando) if sessoes_aguardando else "Nenhuma")
+        #print("\n")
 
     def seleciona_cliente(self) -> int:
         id_cliente = input("ID do cliente que deseja selecionar: ")
