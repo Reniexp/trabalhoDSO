7ab464d18eb1864e45ed4094c2b77b296452dfd7
Reni Ferreira <renirogete.exp@gmail.com>
2024-12-14 22:34:56 -0300

    Implementação Filme Dao

---
+++ DAOs/filme_dao.py
@@ -0,0 +1,23 @@
+from DAOs.dao import DAO
+from entidade.filme import Filme
+
+
+class filmeDAO(DAO):
+    def __init__(self):
+        super().__init__('filme.pkl')
+
+    def add(self, filme: filme):
+        if((filme is not None) and isinstance(filme, Filme) and isinstance(filme.cpf, int)):
+            super().add(filme.cpf, filme)
+
+    def update(self, filme: Filme):
+        if((filme is not None) and isinstance(filme, Filme) and isinstance(filme.cpf, int)):
+            super().update(filme.cpf, filme)
+
+    def get(self, key:int):
+        if isinstance(key, int):
+            return super().get(key)
+
+    def remove(selfself, key:int):
+        if(isinstance(key, int)):
+            return super().remove(key)
